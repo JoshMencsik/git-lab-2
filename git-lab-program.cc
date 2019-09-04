329350dcb95b8e7445e7546b3da9d1c8f73b840f
Josh Mencsik <jmmencsik@gmail.com>
2019-09-04 17:16:40 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Josh Mencsik
+ *        Date: 9 Sept. 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout<<"Hello Git!\n";
+    return 0;
+}// main
